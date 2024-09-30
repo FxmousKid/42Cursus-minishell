f33580b52e45c0142a7269c9ce184cd0842a6239
FxmousKid <iyan005@outlook.com>
2024-09-30 17:49:37 +0200

    Subject: minor updates on error_manager.h and .github/workflows/main.yml
    Details:
        - Added comment for freeing functions in error_manager.h
        - Made main.yml clearer in reagrds to the fact that norm_job
        only checks .c files and not header

--- includes/error_manager.h
+++ includes/error_manager.h
@@ -6,7 +6,7 @@
 /*   By: inazaria <inazaria@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/30 15:09:45 by inazaria          #+#    #+#             */
-/*   Updated: 2024/09/30 15:23:01 by inazaria         ###   ########.fr       */
+/*   Updated: 2024/09/30 17:11:00 by inazaria         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,9 @@
 // Debug Helping
 # define DBG(reason) reason " at line " \
 TOSTRING(__LINE__) " in file " __FILE__ "\n"
+
+// Freeing functions
+
 
 // Error Handling
 
