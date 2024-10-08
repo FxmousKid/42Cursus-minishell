7fa7f088c10323a0568608a3013c942a3d69ee8d
Theo <theo@polgar.fr>
2024-10-08 15:31:25 +0200

    change built in echo and error management on built in

--- src/builtin.c
+++ src/builtin.c
@@ -6,7 +6,7 @@
 /*   By: ptheo <ptheo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/04 20:51:58 by ptheo             #+#    #+#             */
-/*   Updated: 2024/10/07 15:38:29 by ptheo            ###   ########.fr       */
+/*   Updated: 2024/10/08 15:30:50 by ptheo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,34 +14,33 @@
 
 int	builtin_echo(char **arg)
 {
-	if (arg[1] == NULL)
+	int	n;
+	int	i;
+
+	i = 0;
+	n = 1;
+	if (ft_strncmp(arg[0], "-n", ft_strlen(arg[0])))
 	{
-		ft_putstr_fd(arg[0], 1);
+			ft_putstr_fd(arg[0], 1);
+			i = 1;
+			n = 0;
+	}
+	while (arg[i] != NULL)
+	{
+		ft_putstr_fd(arg[i], 1);
+		i++;
+	}
+	if (n)
 		ft_putstr_fd("\n", 1);
-	}
-	else
-	{
-		if (arg[0][1] == 'n')
-			ft_putstr_fd(arg[1], 1);
-		else
-			return (debug(DBG("Wrong option echo")), 0);
-	}
 	return (1);
 }
 
 int	builtin_pwd(int	i)
 {
-	char	buf[i];
+	char	buf[4096];
 
-	if (getcwd(buf, i) == NULL)
-	{
-		if (errno == ERANGE)
-		{
-			builtin_pwd(i + 1);
-			return (1);
-		}
-		return (debug(DBG("Failed to get the path of the working directory")), 0 );
-	}
+	if (getcwd(buf, 4096) == NULL)
+		return (perror(""), debug(DBG("Failed to getcwd()")), 0 );
 	ft_putstr_fd(buf, 1);
 	ft_putstr_fd("\n", 1);
 	return (1);
@@ -50,6 +49,6 @@ int	builtin_pwd(int	i)
 int	builtin_cd(char *path)
 {
 	if (chdir(path) == -1)
-		return (debug(DBG("Failed to cd into the path")), 0);
+		return (perror(""), debug(DBG("Failed to chdir()")), 0);
 	return (0);
 }
