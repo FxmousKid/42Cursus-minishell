62377ca57b736cf7908563d508f6aec6736294a3
Theo <theo@polgar.fr>
2024-10-10 02:02:48 +0200

    fixing norm on starting text

--- includes/dependencies.h
+++ includes/dependencies.h
@@ -6,7 +6,7 @@
 /*   By: ptheo <ptheo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/29 15:36:36 by inazaria          #+#    #+#             */
-/*   Updated: 2024/10/07 17:49:20 by ptheo            ###   ########.fr       */
+/*   Updated: 2024/10/10 02:01:37 by ptheo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,5 +35,71 @@
 # include <time.h>
 # include <sys/ioctl.h>
 # include <termios.h>
+
+# define START_TEXT1 "\e[0;32m          _____                    _____      "
+# define START_TEXT2 "              _____                    _____ 		\n"
+# define START_TEXT3 "         /\\    \\                  /\\    \\         "
+# define START_TEXT4 "         /\\    \\                  /\\    \\	         "
+# define START_TEXT5 "                               ..                \n"
+# define START_TEXT6 "        /::\\    \\                /::\\    \\       "
+# define START_TEXT7 "         /::\\    \\                /::\\____\\	      "
+# define START_TEXT8 "                              .:::::::. 				\n"
+# define START_TEXT9 "       /::::\\    \\              /::::\\    \\       "
+# define START_TEXT10 "       /::::\\    \\              /:::/    /	          "
+# define START_TEXT11 "              .....     .:...:....:-.  				\n"
+# define START_TEXT12 "      /::::::\\    \\            /::::::\\    \\      "
+# define START_TEXT13 "      /::::::\\    \\            /:::/    /	          "
+# define START_TEXT14 "           .........  .:....:-:....===:              \n"
+# define START_TEXT15 "     /:::/\\:::\\    \\          /:::/\\:::\\    \\   "
+# define START_TEXT16 "       /:::/\\:::\\    \\          /:::/    /	      "
+# define START_TEXT17 "              .......:::.:-:...........:==--.        \n"
+# define START_TEXT18 "    /:::/  \\:::\\    \\        /:::/__\\:::\\    \\  "
+# define START_TEXT19 "      /:::/__\\:::\\    \\        /:::/____/	          "
+# define START_TEXT20 "           ......:::...::---:.....---===.            \n"
+# define START_TEXT21 "   /:::/    \\:::\\    \\      /::::\\   \\:::\\    \\"
+# define START_TEXT22 "       \\:::\\   \\:::\\    \\      /::::\\    \\	  "
+# define START_TEXT23 "                    ....::::.:-:.::--:..---=====.\n"
+# define START_TEXT24 "  /:::/    / \\:::\\    \\    /::::::\\   \\:::\\    \\"
+# define START_TEXT25 "    ___\\:::\\   \\:::\\    \\    /::::::\\    \\   ___"
+# define START_TEXT26 "__                        ..  ...=$-=$-.:::--==-:. \n /"
+# define START_TEXT27 ":::/    /   \\:::\\    \\  /:::/\\:::\\   \\:::\\    \\"
+# define START_TEXT28 "  /\\   \\:::\\   \\:::\\    \\  /:::/\\:::\\    \\ /\\"
+# define START_TEXT29 "    \\	                     .:..$=$=$=...::-:.\n"
+# define START_TEXT30 "/:::/____/     \\:::\\____\\/:::/  \\:::\\   \\:::\\____"
+# define START_TEXT31 "\\/::\\   \\:::\\   \\:::\\____\\/:::/  \\:::\\    /::\\"
+# define START_TEXT32 "____\\	                    .::::::.-$-...::.       \n"
+# define START_TEXT33 "\\:::\\    \\      \\::/    /\\::/    \\:::\\  /:::/    "
+# define START_TEXT34 "/\\:::\\   \\:::\\   \\::/    /\\::/    \\:::\\  /:::/  "
+# define START_TEXT35 "  /	                  .:....:::::....::-:::::...   \n"
+# define START_TEXT36 " \\:::\\    \\      \\/____/  \\/____/ \\:::\\/:::/    /"
+# define START_TEXT37 "  \\:::\\   \\:::\\   \\/____/  \\/____/ \\:::\\/:::/   "
+# define START_TEXT38 " /	                .:-:...:::.::::...::-:::::...   \n"
+# define START_TEXT39 "  \\:::\\    \\                       \\::::::/    /    "
+# define START_TEXT40 "\\:::\\   \\:::\\    \\               \\::::::/    /	 "
+# define START_TEXT41 "             -====:...::.....:-:::::::::.....         \n"
+# define START_TEXT42 "   \\:::\\    \\                       \\::::/    /     "
+# define START_TEXT43 " \\:::\\   \\:::\\____\\               \\::::/    /	 "
+# define START_TEXT44 "            .++===-........:--=-..::........          \n"
+# define START_TEXT45 "    \\:::\\    \\                      /:::/    /       "
+# define START_TEXT46 " \\:::\\  /:::/    /               /:::/    /	       "
+# define START_TEXT47 "        :-+=-=-=-::---===. .:.......                \n"
+# define START_TEXT48 "     \\:::\\    \\                    /:::/    /        "
+# define START_TEXT49 "  \\:::\\/:::/    /               /:::/    /	         "
+# define START_TEXT50 "                 -+=====--===-   .........            \n"
+# define START_TEXT51 "      \\:::\\    \\                  /:::/    /         "
+# define START_TEXT52 "   \\::::::/    /               /:::/    /	           "
+# define START_TEXT53 "                  -+=-====-.      .......             \n"
+# define START_TEXT54 "       \\:::\\____\\                /:::/    /          "
+# define START_TEXT55 "    \\::::/    /               /:::/    /	           "
+# define START_TEXT56 "                    .====-.            ..             \n"
+# define START_TEXT57 "        \\::/    /                \\::/    /            "
+# define START_TEXT58 "    \\::/    /                \\::/    /	             "
+# define START_TEXT59 "                    :-.                               \n"
+# define START_TEXT60 "         \\/____/                  \\/____/             "
+# define START_TEXT61 "     \\/____/                  \\/____/ 	\n"
+# define START_TEXT62 "\n"
+# define START_TEXT63 "        							by Iyan NAZARIAN and "
+# define START_TEXT64 "Theo POLGAR\e[0m\n"
+# define START_TEXT65 "\n"
 
 #endif
--- src/prompt.c
+++ src/prompt.c
@@ -6,7 +6,7 @@
 /*   By: ptheo <ptheo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/04 23:15:24 by inazaria          #+#    #+#             */
-/*   Updated: 2024/10/09 19:55:31 by ptheo            ###   ########.fr       */
+/*   Updated: 2024/10/10 02:02:25 by ptheo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,24 @@
 
 void	print_start_text(void)
 {
-	printf("%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s", START_TEXT_1,
-		START_TEXT_2, START_TEXT_3, START_TEXT_4, START_TEXT_5, START_TEXT_6,
-		START_TEXT_7, START_TEXT_8, START_TEXT_9, START_TEXT_10, START_TEXT_11,
-		START_TEXT_12, START_TEXT_13, START_TEXT_14, START_TEXT_15, START_TEXT_16,
-		START_TEXT_17, START_TEXT_18, START_TEXT_19, START_TEXT_20, START_TEXT_21,
-		START_TEXT_22, START_TEXT_23, START_TEXT_24);
+	printf("%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s",
+		START_TEXT1, START_TEXT2, START_TEXT3, START_TEXT4, START_TEXT5,
+		START_TEXT6, START_TEXT7, START_TEXT8, START_TEXT9, START_TEXT10,
+		START_TEXT11, START_TEXT12, START_TEXT13, START_TEXT14, START_TEXT15,
+		START_TEXT16, START_TEXT17, START_TEXT18, START_TEXT19, START_TEXT20,
+		START_TEXT21, START_TEXT22, START_TEXT23, START_TEXT24, START_TEXT25,
+		START_TEXT26, START_TEXT27, START_TEXT28, START_TEXT29, START_TEXT30,
+		START_TEXT31);
+	printf("%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s",
+		START_TEXT32, START_TEXT33, START_TEXT34, START_TEXT35,
+		START_TEXT36, START_TEXT37, START_TEXT38, START_TEXT39, START_TEXT40,
+		START_TEXT41, START_TEXT42, START_TEXT43, START_TEXT44, START_TEXT45,
+		START_TEXT46, START_TEXT47, START_TEXT48, START_TEXT49, START_TEXT50,
+		START_TEXT51, START_TEXT52, START_TEXT53, START_TEXT54, START_TEXT55,
+		START_TEXT56, START_TEXT57, START_TEXT58, START_TEXT59, START_TEXT60,
+		START_TEXT61, START_TEXT62);
+	printf("%s%s%s", START_TEXT63, START_TEXT64, START_TEXT65);
+	
 }
 
 static int	reduce_home_into_tilde(char *cwd, char *new_cwd)
