2d2a46329fd23e92cd8a2cbea1ff522e93d60871
FxmousKid <iyan005@outlook.com>
2024-10-27 17:06:48 +0100

    small changes in parser.c

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -6,10 +6,21 @@
 /*   By: inazaria <inazaria@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/26 18:36:25 by inazaria          #+#    #+#             */
-/*   Updated: 2024/10/26 18:37:07 by inazaria         ###   ########.fr       */
+/*   Updated: 2024/10/27 17:06:10 by inazaria         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+/* the Goal here is to analyze syntax.
+ * 1) Check for definitive syntaxically unfixable tokens : "echo |||"
+ *	  cash : parse error near `META_CHARACTER' 
+ * 2) Check for wrong but syntaxically fixable tokens, and prompt the
+ *    user to finish them : echo |
+ *    pipe> 
+ * 3) Fill the AST for the execution
+ * */
 
+
+/* 
+ * */
