337889a5b46c245955c40ba6317c0fe12951e3d8
FxmousKid <iyan005@outlook.com>
2024-11-13 12:04:53 +0100

    same as last commit

--- src/parser.c
+++ src/parser.c
@@ -6,32 +6,36 @@
 /*   By: inazaria <inazaria@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/26 18:36:25 by inazaria          #+#    #+#             */
-/*   Updated: 2024/11/12 18:04:14 by inazaria         ###   ########.fr       */
+/*   Updated: 2024/11/13 11:38:45 by inazaria         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "macros.h"
 #include "minishell.h"
 
-static void	handle_partial_quote_or_pipe(t_lexer *lex)
+static void	handle_partial_prompt(t_lexer *lex)
 {
-	char	tmp[4096];
+	char	tmp[PATH_MAX];
 
-	ft_bzero(tmp, 4096);
-	ft_strlcpy(tmp, lex->input, 4096);
+	ft_bzero(tmp, sizeof(tmp));
+	ft_strlcpy(tmp, lex->input, PATH_MAX);
+	
+	int c = lex->lexem_count;
+	lex->lexem_count = c + 1;
+	print_lexems(lex);
+	lex->lexem_count = c;
+
 	free_lex(lex);
-	lexer(lex, ft_strdup(tmp));
+	ft_bzero(lex, sizeof(t_lexer));
+
+	lexer(lex, tmp);
 }
 
 static bool	prompt_for_partials(t_data *data, t_lexer *lex)
 {
-	if (prompt_for_partial_sq_dq(lex))
+	if (prompt_for_partial_sq_dq(lex) ^ prompt_for_partial_pipe(lex))
 	{
-		handle_partial_quote_or_pipe(lex);
-		return (parser(data, lex));
-	}
-	if (prompt_for_partial_pipe(lex))
-	{
-		handle_partial_quote_or_pipe(lex);
+		handle_partial_prompt(lex);
 		return (parser(data, lex));
 	}
 	return (true);
@@ -45,25 +49,22 @@ static bool	prompt_for_partials(t_data *data, t_lexer *lex)
  * 3) Read heredocs
  * 4) expand variables
  * 5) Fill command structures
+ *
+ * on failure, return the exit code of the appropriate failure type
+ * e.g. 2 for syntax error.
  * */
 
-bool	parser(t_data *data, t_lexer *lex)
+int	parser(t_data *data, t_lexer *lex)
 {
 	(void)	data;
 	char	*heredocs_content[MAX_HEREDOCS];
-	char	tmp[4096];
 
 	ft_bzero(heredocs_content, sizeof(heredocs_content));
-	ft_bzero(tmp, sizeof(tmp));
 	if (!search_parse_error(lex))
-		return (debug(DBG("Failed to search_parse_error()")), false);	
+		return (debug(DBG("Failed to search_parse_error()")), 2);	
 	if (!prompt_for_partials(data, lex))
 		return (debug(DBG("Failed to prompt_for_partials()")), false);
 	if (!read_and_expand_heredocs(lex, heredocs_content))
 		return (debug(DBG("Failed to read_heredocs()")), false);
-
-
-	print_split(data->env->paths);
-
 	return (true);
 }
